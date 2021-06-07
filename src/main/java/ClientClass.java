ba75de0157fe5be7412f0718fce15c71c410d367
isberg <sAYgDRexgBgM3rt>
2021-06-07 13:54:36 +0800

    3

--- src/main/java/ClientClass.java
+++ src/main/java/ClientClass.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class ClientClass {
+
+    public static void clientClass() {
+        String host= "localhost";
+        int port= 8081;
+        try (Socket clientSocket = new Socket(host, port);
+             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+            out.println("TEST LOCALHOST CONNECTION");
+            String resp = in.readLine();
+            System.out.println(resp);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
